42234985492994860d36bc19ed655e4642d3d7bc
masud-rana44 <mdmasudrana4488@gmail.com>
2023-10-26 03:10:49 +0600

    Installed tailwind

---
+++ src/components/AppLayout.jsx
@@ -0,0 +1,5 @@
+const AppLayout = () => {
+  return <div className="text-2xl font-bold text-indigo-600">AppLayout</div>;
+};
+
+export default AppLayout;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,31 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "./index.css";
+import AppLayout from "./components/AppLayout";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter([
+  {
+    element: <AppLayout />,
+    children: [
+      {
+        path: "/",
+        element: <div>Home</div>,
+      },
+      {
+        path: "/about",
+        element: <div>About</div>,
+      },
+      {
+        path: "/contact",
+        element: <div>Contact</div>,
+      },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />{" "}
+  </React.StrictMode>
+);
